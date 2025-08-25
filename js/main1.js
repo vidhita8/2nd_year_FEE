b971080e493280f7bc60dfe01a7e66a2353ba9af
vidhita8 <guptavidhita@gmail.com>
2025-08-25 15:24:10 +0530

    till_object_js

---
+++ js/main1.js
@@ -0,0 +1,24 @@
+// console.log("Hello, World!");
+
+// //create a variable celsius with a number value convert it to fahrenhite using the formula
+// let celsius = 25;
+// let fahrenheit = (celsius * 9/5) + 32;
+// console.log(celsius + "°C is " + fahrenheit + "°F");
+
+// let principal=1000;
+// let rate=5;
+// let time=3;
+// let interest= (principal * rate * time) / 100;
+// console.log("The interest is: " + interest);
+
+
+// let firstName="Vidhita";
+// let lastNName="Gupta";
+// let fullNName=firstName+" "+lastNName;
+// console.log("My name is: " + fullNName);
+
+// let radius=5;
+// let area=Math.PI*radius*radius;
+// console.log("The area of the circle is: " + area);
+
+
